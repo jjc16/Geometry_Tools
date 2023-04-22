79ed05a5ab4225c98c1fcf05d27c7601154504c7
jjc16 <jjc16_1@yahoo.com>
2023-04-22 03:32:12 +0000

    pass

--- tests/hypercube_test.py
+++ tests/hypercube_test.py
@@ -39,6 +39,7 @@ class TestHyperCube(unittest.TestCase):
         # Test cases for find_all_hypercubes
         # list_ = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3], [3,1], [3,2], [3,3], [4,1], [4,2], [4,3], [5,1], [5,2], [5,3]]
         # self.assertEqual(find_all_hypercubes(list_),'11, {1: 8, 2: 3}')
+        pass
     
 if __name__ == '__main__':
     unittest.main()
